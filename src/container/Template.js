210680cee412e9fec6047c50224989c0250c8293
Ayush Pandey <intern.ayush.pandey@redcarpetup.com>
2018-08-16 14:44:30 +0530

    first-heroku-commit

--- src/container/Template.js
+++ src/container/Template.js
@@ -1,17 +1,29 @@
 import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+//import RaisedButton from 'material-ui/RaisedButton'
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
+
+injectTapEventPlugin()
 
 class Template extends Component{
 
     render(){
         return(
+            <MuiThemeProvider>
         <div>
             <header>
                 <h1>TicTacToe</h1>
+                <Button variant="contained" color="primary" href='profile'>
+        Primary
+      </Button>
             </header>
             <main>
                 {this.props.children}
              </main>
             </div>
+           </MuiThemeProvider> 
             )
     }
 }
